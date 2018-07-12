12c4426276a66af2430e99b495254859db007f1c
Samuel Louvan <slouvan@cs.stonybrook.edu>
2018-07-12 11:15:03 +0200

    Fix parameter file for MTL MIT Restaurant

--- submit_job_rerun_with_best_dropout.sh
+++ submit_job_rerun_with_best_dropout.sh
@@ -9,4 +9,4 @@ python Train_MTL_Sequence_Tagging.py -i input/MIT_Restaurant_CoNLL -d MTL_MIT_Re
 wait
 python Train_MTL_Sequence_Tagging.py -i input/ATIS_OntoNotes_NW -d MTL_ATIS_OntoNotes_NW_BestDropout_12_July -p params/MTL_MIT_Movie_OntoNotes_NW_BestParam
 wait
-python Train_MTL_Sequence_Tagging.py -i input/MIT_Restaurant_OntoNotes_NW -d MTL_MIT_Restaurant_OntoNotes_NW_BestDropout_12_July -p params/MTL_MIT_Restaurant_OntoNotes_NW
+python Train_MTL_Sequence_Tagging.py -i input/MIT_Restaurant_OntoNotes_NW -d MTL_MIT_Restaurant_OntoNotes_NW_BestDropout_12_July -p params/MTL_MIT_Restaurant_OntoNotes_NW_BestParam
